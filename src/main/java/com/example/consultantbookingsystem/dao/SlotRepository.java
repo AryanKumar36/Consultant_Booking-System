ad66beca4efc308315db6471993cf57401789a3f
AryanKumar36 <ak24012000@gmail.com>
2024-03-12 23:17:25 +0530

    "Query for slot written"

--- src/main/java/com/example/consultantbookingsystem/dao/SlotRepository.java
+++ src/main/java/com/example/consultantbookingsystem/dao/SlotRepository.java
@@ -2,6 +2,20 @@ package com.example.consultantbookingsystem.dao;
 
 import com.example.consultantbookingsystem.entity.Slot;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.sql.Time;
+import java.util.List;
 
 public interface SlotRepository extends JpaRepository<Slot, Long> {
+
+    @Query("SELECT s FROM Slot s WHERE s.consultantId = :consultantId " +
+    "AND s.dayOfWeek= :dayOfWeek " +
+    "AND ((s.startTime<= :newEndTime  AND :newStartTime<= s.endTime) OR " +
+    "     (s.startTime<= :newStartTime AND :newEndTime<= s.endTime))")
+    List<Slot> findOverlappingSlotsByDay(@Param("consultantId") Long consultantId,
+                                         @Param("dayOfWeek") String dayOfWeek,
+                                         @Param("newStartTime") Time newStartTime,
+                                         @Param("newEndTime") Time newEndTime);
 }
