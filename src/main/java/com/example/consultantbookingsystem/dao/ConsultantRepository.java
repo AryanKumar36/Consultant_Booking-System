a712d1b86d91481390b774e13f2c939df7823d6c
AryanKumar36 <ak24012000@gmail.com>
2024-03-13 02:02:03 +0530

    updated dao

--- src/main/java/com/example/consultantbookingsystem/dao/ConsultantRepository.java
+++ src/main/java/com/example/consultantbookingsystem/dao/ConsultantRepository.java
@@ -3,6 +3,11 @@ package com.example.consultantbookingsystem.dao;
 import com.example.consultantbookingsystem.entity.Consultant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface ConsultantRepository extends JpaRepository<Consultant, Long> {
+
+    @Override
+    boolean existsById(Long theId);
 }
